e141711f4dab45d40be96994d46036bdf14fa2bb
Lelouch Xen <73176845+lelouchxx@users.noreply.github.com>
2021-03-31 16:50:18 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print('hello world lol!')
+
+testdict = {}
+number =4
+serial = 0
+text = 'key '
+
+for i in range(number):
+  serial +=1
+  testdict.update({f'{text}{serial}':i})                 
+  
+  
